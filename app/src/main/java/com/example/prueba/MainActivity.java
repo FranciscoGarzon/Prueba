50347adab5feb3b36a5a310a63fb306e47f346fb
Francisco <franciscogarzonmoreno@gmail.com>
2024-02-24 00:34:30 -0500

    se agregaron funciones

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -4,28 +4,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText1, editText2, editTextResult;
+    private EditText editText1, editText2;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Referencias a los EditText y al Button
+        // Referencias a los EditText, TextView y al Button
         editText1 = findViewById(R.id.editTextText);
         editText2 = findViewById(R.id.editTextText2);
-        editTextResult = findViewById(R.id.editTextText3); // Cambié el ID a editTextText3
+        textViewResult = findViewById(R.id.textView2);
         Button button = findViewById(R.id.button);
 
         // Configuración del OnClickListener para el botón "Calcular"
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtener los valores de editTextText y textView7 como String
+                // Obtener los valores de editTextText y editTextText2 como String
                 String value1Str = editText1.getText().toString();
                 String value2Str = editText2.getText().toString();
 
@@ -36,10 +38,9 @@ public class MainActivity extends AppCompatActivity {
                 // Calcular la suma
                 int total = value1 + value2;
 
-                // Mostrar el resultado en editTextText2
-                editTextResult.setText(String.valueOf(total));
+                // Mostrar el resultado en textView2
+                textViewResult.setText(String.valueOf(total));
             }
         });
     }
 }
-
