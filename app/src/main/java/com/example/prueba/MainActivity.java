0f8e5070516fc5cd0b08bf3cb543ef3b0cf83cf6
Francisco <franciscogarzonmoreno@gmail.com>
2024-02-24 00:24:52 -0500

    cambio dre botones de texto

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -3,31 +3,31 @@ package com.example.prueba;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView6, textView7, textView2;
+    private EditText editText1, editText2, editTextResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Referencias a los TextViews y al Button
-        textView6 = findViewById(R.id.textView6);
-        textView7 = findViewById(R.id.textView7);
-        textView2 = findViewById(R.id.textView2);
+        // Referencias a los EditText y al Button
+        editText1 = findViewById(R.id.editTextText);
+        editText2 = findViewById(R.id.editTextText2);
+        editTextResult = findViewById(R.id.editTextText3); // Cambié el ID a editTextText3
         Button button = findViewById(R.id.button);
 
         // Configuración del OnClickListener para el botón "Calcular"
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtener los valores de textView6 y textView7 como String
-                String value1Str = textView6.getText().toString();
-                String value2Str = textView7.getText().toString();
+                // Obtener los valores de editTextText y textView7 como String
+                String value1Str = editText1.getText().toString();
+                String value2Str = editText2.getText().toString();
 
                 // Convertir los valores a números enteros
                 int value1 = Integer.parseInt(value1Str);
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 // Calcular la suma
                 int total = value1 + value2;
 
-                // Mostrar el resultado en textView2
-                textView2.setText(String.valueOf(total));
+                // Mostrar el resultado en editTextText2
+                editTextResult.setText(String.valueOf(total));
             }
         });
     }
